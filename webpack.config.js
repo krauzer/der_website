fe452ef69c5c1341eaeedb4cdb3f0755a5cb3609
Dylan Krause <dykrause@gmail.com>
2015-11-14 23:44:08 -0500

    Change Sass loader extension to look for scss

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         loader: 'babel?presets[]=es2015'
       },
       {
-        test: /\.sass$/,
+        test: /\.scss$/,
         loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
       }
     ]
